73e115b3235777f9fe5dfa8767425de6abbca639
Ruba <109694075+1rubasaleh@users.noreply.github.com>
2026-04-20 18:02:55 +0300

    feat: responsive HERO

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,44 @@
+import hero from "../assets/hero.png";
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <> </>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-[90vh] relative w-full overflow-hidden">
+      {/* Background Image */}
+      <img
+        src={hero}
+        alt="hero"
+        className="absolute inset-0 w-full h-full object-cover object-center"
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/20"></div>
+
+      {/* Content */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white px-6">
+        
+      
+        <h1 className="text-3xl sm:text-5xl md:text-7xl font-vibes text-pink-500 animate-float font-bold leading-tight">
+          Glow Your Beauty
+        </h1>
+
+        <p className="mt-2 text-[10px] xs:text-xs sm:text-sm md:text-lg opacity-80 tracking-wide uppercase">
+          Discover the best cosmetics products
+        </p>
+
+       
+        <button 
+          onClick={() => navigate("/products")}
+          className="mt-6 bg-pink-500 text-white px-5 py-2 md:px-8 md:py-3 rounded-lg text-xs md:text-base transition-all duration-300 
+                     hover:scale-110 hover:shadow-[0_0_20px_rgba(236,72,153,0.6)] 
+                     active:scale-95 font-medium shadow-md"
+        >
+          Shop Now
+        </button>
+
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+            vibes: ["Great Vibes", "cursive"],
+
       },
-    },
+keyframes: {
+  float: {
+    '0%, 100%': { transform: 'translateY(0)' },
+    '50%': { transform: 'translateY(-10px)' },
   },
+},
+animation: {
+  float: 'float 4s ease-in-out infinite',
+}
+   } },
   plugins: [],
 };
